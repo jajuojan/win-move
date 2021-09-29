c260c090c063294f9132a9ffbce3b7eb44dca4fb
Jani Ojanen <jajuojan@gmail.com>
2021-09-30 01:08:20 +0300

    Move calculate_windows_rect into logic.rs

--- src/mswindows.rs
+++ src/mswindows.rs
@@ -90,51 +90,6 @@ pub fn get_window_margin(foreground_window: HWND) -> WindowBorderSize {
         bottom: r.bottom - r2.bottom,
     }
 }
-
-// 1px horizontal border seems to happen even when taking extended frame into account,
-// increase windows' width by 1px to compensate and move right windows left by 1px
-// The same for vertical borders seems to happen when the windows' vertical extended frame > 0
-// Take this into account as well (curently +2px in height)
-// TODO: Split the compensation of vertical border between top/bottom windows
-// TODO: Some windows don't seem to have extended frame like 'VS Code', do these have border?
-// TODO: Test how this works with hidden taskbar
-pub fn calculate_windows_rect(
-    monitor_info: MonitorInfo,
-    window_margin: WindowBorderSize,
-    pressed_key: HotKeyButtons,
-) -> WindowTarget {
-    let left = match pressed_key {
-        HotKeyButtons::RightBottom | HotKeyButtons::RightMiddle | HotKeyButtons::RightTop => {
-            (monitor_info.width / 2) - 1
-        }
-        _ => 0,
-    } + monitor_info.x_offset;
-
-    let top = match pressed_key {
-        HotKeyButtons::LeftBottom | HotKeyButtons::Bottom | HotKeyButtons::RightBottom => {
-            monitor_info.height / 2
-        }
-        _ => 0,
-    } + monitor_info.y_offset;
-
-    let width = match pressed_key {
-        HotKeyButtons::Bottom | HotKeyButtons::Top => monitor_info.width,
-        _ => (monitor_info.width / 2) + 1,
-    };
-
-    let height = match pressed_key {
-        HotKeyButtons::LeftMiddle | HotKeyButtons::RightMiddle => monitor_info.height,
-        _ => monitor_info.height / 2,
-    };
-
-    WindowTarget {
-        left: left + window_margin.left,
-        top,
-        width: width + window_margin.right - window_margin.left,
-        height: height + window_margin.bottom + (if window_margin.bottom > 0 { 2 } else { 0 }),
-    }
-}
-
 pub fn get_monitor_info(foreground_window: HWND) -> MonitorInfo {
     let monitor;
     unsafe {
