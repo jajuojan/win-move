3a6c03cee0bebf52af18ebdabc66e6a0eb667e30
Jani Ojanen <jajuojan@gmail.com>
2022-04-28 23:54:25 +0300

    fix: #44 Add application manifest with DPI awareness

--- build.rs
+++ build.rs
@@ -1,19 +1,26 @@
 fn main() {
-    windows::build! {
-        Windows::UI::Color,
-        Windows::UI::Colors,
-        Windows::Win32::Foundation::*,
-        Windows::Win32::Graphics::Dwm::*,
-        Windows::Win32::Graphics::Dwm::DwmGetWindowAttribute,
-        Windows::Win32::Graphics::Gdi::*,
-        Windows::Win32::Graphics::Gdi::GetMonitorInfoW,
-        Windows::Win32::Graphics::Gdi::MonitorFromWindow,
-        Windows::Win32::UI::KeyboardAndMouseInput::*,
-        Windows::Win32::UI::KeyboardAndMouseInput::GetActiveWindow,
-        Windows::Win32::UI::KeyboardAndMouseInput::RegisterHotKey,
-        Windows::Win32::UI::WindowsAndMessaging::*,
-        Windows::Win32::UI::WindowsAndMessaging::GetForegroundWindow,
-        Windows::Win32::UI::WindowsAndMessaging::GetWindowRect,
-        Windows::Win32::UI::WindowsAndMessaging::MoveWindow,
-    };
+    #[cfg(windows)]
+    {
+        windows::build! {
+            Windows::UI::Color,
+            Windows::UI::Colors,
+            Windows::Win32::Foundation::*,
+            Windows::Win32::Graphics::Dwm::*,
+            Windows::Win32::Graphics::Dwm::DwmGetWindowAttribute,
+            Windows::Win32::Graphics::Gdi::*,
+            Windows::Win32::Graphics::Gdi::GetMonitorInfoW,
+            Windows::Win32::Graphics::Gdi::MonitorFromWindow,
+            Windows::Win32::UI::KeyboardAndMouseInput::*,
+            Windows::Win32::UI::KeyboardAndMouseInput::GetActiveWindow,
+            Windows::Win32::UI::KeyboardAndMouseInput::RegisterHotKey,
+            Windows::Win32::UI::WindowsAndMessaging::*,
+            Windows::Win32::UI::WindowsAndMessaging::GetForegroundWindow,
+            Windows::Win32::UI::WindowsAndMessaging::GetWindowRect,
+            Windows::Win32::UI::WindowsAndMessaging::MoveWindow,
+        };
+
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest_file("win-move.exe.manifest");
+        res.compile().unwrap();
+    }
 }
