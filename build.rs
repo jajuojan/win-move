dad6b9d77d8a3a8d8967dc78ed2821ffda771756
Jani Ojanen <jajuojan@gmail.com>
2022-05-09 21:39:50 +0300

    chore: Update to windows 0.36.1

--- build.rs
+++ build.rs
@@ -1,24 +1,6 @@
 fn main() {
     #[cfg(windows)]
     {
-        windows::build! {
-            Windows::UI::Color,
-            Windows::UI::Colors,
-            Windows::Win32::Foundation::*,
-            Windows::Win32::Graphics::Dwm::*,
-            Windows::Win32::Graphics::Dwm::DwmGetWindowAttribute,
-            Windows::Win32::Graphics::Gdi::*,
-            Windows::Win32::Graphics::Gdi::GetMonitorInfoW,
-            Windows::Win32::Graphics::Gdi::MonitorFromWindow,
-            Windows::Win32::UI::KeyboardAndMouseInput::*,
-            Windows::Win32::UI::KeyboardAndMouseInput::GetActiveWindow,
-            Windows::Win32::UI::KeyboardAndMouseInput::RegisterHotKey,
-            Windows::Win32::UI::WindowsAndMessaging::*,
-            Windows::Win32::UI::WindowsAndMessaging::GetForegroundWindow,
-            Windows::Win32::UI::WindowsAndMessaging::GetWindowRect,
-            Windows::Win32::UI::WindowsAndMessaging::MoveWindow,
-        };
-
         let mut res = winres::WindowsResource::new();
         res.set_manifest_file("win-move.exe.manifest");
         res.compile().unwrap();
