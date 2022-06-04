097df9032245362bddd44991ec842c87143ec35c
Jani Ojanen <jajuojan@gmail.com>
2022-06-04 10:25:32 +0300

    refactor: Take enum_from_primitive into use

--- src/hotkey_action.rs
+++ src/hotkey_action.rs
@@ -1,3 +1,6 @@
+extern crate num;
+
+enum_from_primitive! {
 #[derive(Debug, Copy, Clone, PartialOrd, PartialEq)]
 pub enum HotKeyAction {
     // Move window to specified location
@@ -58,64 +61,4 @@ pub enum HotKeyAction {
     DecreaseWindowSizeTowardsTopHistoryAware = 6007,
     DecreaseWindowSizeTowardsRightTopHistoryAware = 6008,
 }
-
-impl From<u32> for HotKeyAction {
-    fn from(value: u32) -> Self {
-        match value {
-            // TODO: there's probably a better way to do this
-            1001 => HotKeyAction::MoveWindowToLeftBottom,
-            1002 => HotKeyAction::MoveWindowToBottom,
-            1003 => HotKeyAction::MoveWindowToRightBottom,
-            1004 => HotKeyAction::MoveWindowToLeftMiddle,
-            1006 => HotKeyAction::MoveWindowToRightMiddle,
-            1007 => HotKeyAction::MoveWindowToLeftTop,
-            1008 => HotKeyAction::MoveWindowToTop,
-            1009 => HotKeyAction::MoveWindowToRightTop,
-
-            2001 => HotKeyAction::MinimizeWindow,
-            2002 => HotKeyAction::MaximizeWindow,
-            2003 => HotKeyAction::MoveWindowToLeftScreen,
-            2004 => HotKeyAction::MoveWindowToRightScreen,
-            2005 => HotKeyAction::MoveWindowToLeftScreenContinuous,
-            2006 => HotKeyAction::MoveWindowToRightScreenContinuous,
-
-            3001 => HotKeyAction::IncreaseWindowSizeTowardsLeftBottom,
-            3002 => HotKeyAction::IncreaseWindowSizeTowardsBottom,
-            3003 => HotKeyAction::IncreaseWindowSizeTowardsRightBottom,
-            3004 => HotKeyAction::IncreaseWindowSizeTowardsLeftMiddle,
-            3005 => HotKeyAction::IncreaseWindowSizeTowardsRightMiddle,
-            3006 => HotKeyAction::IncreaseWindowSizeTowardsLeftTop,
-            3007 => HotKeyAction::IncreaseWindowSizeTowardsTop,
-            3008 => HotKeyAction::IncreaseWindowSizeTowardsRightTop,
-
-            4001 => HotKeyAction::DecreaseWindowSizeTowardsLeftBottom,
-            4002 => HotKeyAction::DecreaseWindowSizeTowardsBottom,
-            4003 => HotKeyAction::DecreaseWindowSizeTowardsRightBottom,
-            4004 => HotKeyAction::DecreaseWindowSizeTowardsLeftMiddle,
-            4005 => HotKeyAction::DecreaseWindowSizeTowardsRightMiddle,
-            4006 => HotKeyAction::DecreaseWindowSizeTowardsLeftTop,
-            4007 => HotKeyAction::DecreaseWindowSizeTowardsTop,
-            4008 => HotKeyAction::DecreaseWindowSizeTowardsRightTop,
-
-            5001 => HotKeyAction::IncreaseWindowSizeTowardsLeftBottomHistoryAware,
-            5002 => HotKeyAction::IncreaseWindowSizeTowardsBottomHistoryAware,
-            5003 => HotKeyAction::IncreaseWindowSizeTowardsRightBottomHistoryAware,
-            5004 => HotKeyAction::IncreaseWindowSizeTowardsLeftMiddleHistoryAware,
-            5005 => HotKeyAction::IncreaseWindowSizeTowardsRightMiddleHistoryAware,
-            5006 => HotKeyAction::IncreaseWindowSizeTowardsLeftTopHistoryAware,
-            5007 => HotKeyAction::IncreaseWindowSizeTowardsTopHistoryAware,
-            5008 => HotKeyAction::IncreaseWindowSizeTowardsRightTopHistoryAware,
-
-            6001 => HotKeyAction::DecreaseWindowSizeTowardsLeftBottomHistoryAware,
-            6002 => HotKeyAction::DecreaseWindowSizeTowardsBottomHistoryAware,
-            6003 => HotKeyAction::DecreaseWindowSizeTowardsRightBottomHistoryAware,
-            6004 => HotKeyAction::DecreaseWindowSizeTowardsLeftMiddleHistoryAware,
-            6005 => HotKeyAction::DecreaseWindowSizeTowardsRightMiddleHistoryAware,
-            6006 => HotKeyAction::DecreaseWindowSizeTowardsLeftTopHistoryAware,
-            6007 => HotKeyAction::DecreaseWindowSizeTowardsTopHistoryAware,
-            6008 => HotKeyAction::DecreaseWindowSizeTowardsRightTopHistoryAware,
-
-            _ => panic!("Unknown value: {}", value),
-        }
-    }
 }
